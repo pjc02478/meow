463a4ddedff4322de3686a7df40dd4305f1f39f3
Park June Chul <pjc0247@naver.com>
2021-09-28 17:32:22 +0900

    fix: withSpinner now accepts SpinnerComponent

--- src/hoc/withSpinner.tsx
+++ src/hoc/withSpinner.tsx
@@ -1,12 +1,21 @@
 import React from'react';
 import { ActivityIndicator } from 'react-native-paper';
 
-export const withSpinner = (Component: React.FC) => (props: any) => {
+export const withSpinner = (Component: React.FC, SpinnerComponent?: React.FC) => (props: any) => {
   return (
     <React.Suspense
-      fallback={<ActivityIndicator size="large" color="black" />}
+      fallback={SpinnerComponent || DefaultSpinner}
     >
       <Component {...props} />
     </React.Suspense>
+  );
+};
+
+const DefaultSpinner = () => {
+  return (
+    <ActivityIndicator
+      size="large"
+      color="black"
+    />
   );
 };
