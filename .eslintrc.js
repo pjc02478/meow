ec98124123b5230fec31dc1ce6ecf89962dbecdc
Park June Chul <pjc0247@naver.com>
2021-09-30 02:28:11 +0900

    add: eslint + airbnb rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    'project': './tsconfig.json'
+  },
+  env: {
+    'node': true
+  },
+  extends: ['airbnb'],
+  rules: {
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2540#issuecomment-692505191
+    'no-use-before-define': 0,
+
+    'new-cap': 0,
+    'object-curly-newline': 0,
+    'arrow-body-style': 0,
+    'import/prefer-default-export': 0,
+    'no-empty-pattern': 0,
+    'linebreak-style': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 0,
+  }
 };
