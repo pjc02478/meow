f2b239502ca0c48337e637b7584a48511861b833
Park June Chul <pjc0247@naver.com>
2021-09-29 22:22:51 +0900

    fix: broken test on github ci

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['__tests__/catapi'],
   "transformIgnorePatterns": [
-    'node_modules/(?!(jest-)?@react-native|react-native|react-(native|universal|navigation)-(.*)|@react-native-community/(.*)|@react-navigation/(.*)|bs-platform|(@[a-zA-Z]+/)?(bs|reason|rescript)-(.*)+)'
+    'node_modules/(?!(jest-)?@react-navigation|@react-native|react-native|react-(native|universal|navigation)-(.*)|@react-native-community/(.*)|@react-navigation/(.*)|bs-platform|(@[a-zA-Z]+/)?(bs|reason|rescript)-(.*)+)'
   ],
   setupFiles: [
     './jest/setup.js'
