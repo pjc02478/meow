67639aef544678a6b1bd6faf263e8fa11bf3d96c
Park June Chul <pjc0247@naver.com>
2021-09-30 02:58:08 +0900

    refactor: cache key

--- src/data/withPrefetch.tsx
+++ src/data/withPrefetch.tsx
@@ -5,31 +5,33 @@ import { IData, IInfiniteDataProvider } from 'data';
 const prefecthData = {
 } as Record<string, any[]>;
 
+const cacheKey = (dataProvider: new () => any) => {
+  return `__cache_${dataProvider.name}`;
+};
+
 export const prefetchFirstPage = async <T extends IInfiniteDataProvider<TData>, TData extends IData>(
   dataProvider: new () => T,
   pageSize: number,
 ) => {
   const instance = new dataProvider();
-  const cacheKey = `__cache_${dataProvider.name}`;
 
   try {
     const data = await instance.get(pageSize, 0);
-    prefecthData[cacheKey] = data;
+    prefecthData[cacheKey(dataProvider)] = data;
   } catch(e) {
     console.error(e);
   }
 };
-
 export const withPrefetch = <T extends IInfiniteDataProvider<TData>, TData extends IData>(
   dataProvider: new () => T,
 ) => {
   const instance = new dataProvider();
-  const cacheKey = `__cache_${dataProvider.name}`;
+  const key = cacheKey(dataProvider);
 
   return class EnhancedDataProvider implements IInfiniteDataProvider<TData> {
     async get(pageSize: number, page: number) {
-      if (!!prefecthData[cacheKey] && page === 0)
-        return prefecthData[cacheKey].slice(0, pageSize);
+      if (!!prefecthData[key] && page === 0)
+        return prefecthData[key].slice(0, pageSize);
       return await instance.get(pageSize, page);
     }
     async remove(id: number) {
